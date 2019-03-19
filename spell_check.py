ce8cb9a8281004c69c7b3a22320a9d09087d0824
Andrey Komendantov <9400878@gmail.com>
2019-03-19 18:07:58 +0300

    Реализация алгоритма проверки правописания русскоязычных слов

--- spell_check.py
+++ spell_check.py
@@ -1,18 +1,15 @@
-import spell_check
+import dict_rus
 
 # string_test = "Пришло теплое лето. На лисной опушки распускаюца колоколчики, незабутки, шыповник. Белые ромашки пратягивают к сонцу свои нежные лепески. Вылитают из уютных гнёзд птинцы. У зверей взраслеет смена. Мидвежата старше всех."
 
-bed_words = []
+bad_words = []
 
-string_test = input("Введите строку: ")
+string_test = input("Введите текст: ")
 string_test = string_test.split()
 for i in range(len(string_test)):
     string_test[i] = string_test[i].strip(".,\"\'")
-    # print(string_test[i])
 
-    if string_test[i].lower() not in spell_check.russ:
-        # print("no")
-        bed_words.append(string_test[i])
-        # print(string_test[i])
+    if string_test[i].lower() not in dict_rus.russ:
+        bad_words.append(string_test[i])
 
-print(bed_words)
+print(bad_words)
